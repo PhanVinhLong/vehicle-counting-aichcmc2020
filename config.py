7ade143430dc0d29e9a00ff4aa072ae79d2df760
hitonasi <longpv.se@gmail.com>
2020-08-30 23:08:14 +0700

    add yolo pretrained config

--- config.py
+++ config.py
@@ -21,6 +21,17 @@ detector = {
 		'batchsize': 20,
 		'confidencethreshold': 0.5
 	},
+		'detector-yolo-pretrain': {
+		'type': 'yolo',
+		'classesmap': [[1, 3], [2], [5], [7]],
+		'classnames': ['1', '2', '3', '4'],
+		'originclassnames': ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'sofa', 'pottedplant', 'bed', 'diningtable', 'toilet', 'tvmonitor', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'],
+		'originclassnames': ['loai 1', 'loai 2', 'loai 3', 'loai 4', 'loai 5'],
+		'cfgfile': 'yolov4/cfg/yolov4.cfg',
+		'weightfile': 'yolov4/yolov4.weights',
+		'classnamefile': 'yolov4/data/coco.names',
+		'batchsize': 4
+	}
 }
 
 config = {
